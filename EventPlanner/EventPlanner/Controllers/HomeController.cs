01187f69a93015a23c92ca13d174e8ddad5ba166
Robbe Bryssinck <robbebryssinck@gmail.com>
2020-03-05 10:05:09 +0100

    Clean template

--- EventPlanner/EventPlanner/Controllers/HomeController.cs
+++ EventPlanner/EventPlanner/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace EventPlanner.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
