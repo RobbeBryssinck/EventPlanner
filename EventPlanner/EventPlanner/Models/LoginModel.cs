668a3c0fab824dc4169699c1cdb25a1704c17ceb
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-03-16 10:23:22 +0100

    login page

---
+++ EventPlanner/EventPlanner/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
