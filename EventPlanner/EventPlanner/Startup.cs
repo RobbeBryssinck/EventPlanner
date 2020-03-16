00d37a177fca48e2082d13372eb742d471074d33
dennycox <denny.cox@chello.nl>
2020-03-16 11:18:22 +0100

    Revert "Merge branch 'database-seeder'"
    This reverts commit 760aea460dc5c5b24a0d220a5dcae1fe5b577bdc, reversing
    changes made to fae4b17182e6944f8a65e09a25a77211e61c137b.

--- EventPlanner/EventPlanner/Startup.cs
+++ EventPlanner/EventPlanner/Startup.cs
@@ -24,9 +24,6 @@ namespace EventPlanner
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddDbContext<EventPlannerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
