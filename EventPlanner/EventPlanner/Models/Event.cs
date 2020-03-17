71fcb627cdd403593b4bb586fa83729267761bcc
dennycox <denny.cox@chello.nl>
2020-03-17 17:17:17 +0100

    Updated Model. Database seeder now works

--- EventPlanner/EventPlanner/Models/Event.cs
+++ EventPlanner/EventPlanner/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,21 @@ namespace EventPlanner.Models
 {
     public class Event
     {
-        public string EventId { get; set; }
-        public string AccountId { get; set; }
+        [Key]
+        public int EventId { get; set; }
+
         public string EventName { get; set; }
+
         public DateTime Date { get; set; }
+
         public int VisitorLimit { get; set; }
+
         public string Description { get; set; }
+
         public string Location { get; set; }
+
         public string Email { get; set; }
+
         public string ImageSrc { get; set; }
     }
 }
