bfcda66211e8aa3c2d107315e1a98249a024e7f8
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-03-19 09:31:32 +0100

    Finished layout

--- EventPlanner/EventPlanner/Models/LoginModel.cs
+++ EventPlanner/EventPlanner/Models/LoginModel.cs
@@ -7,11 +7,6 @@ namespace EventPlanner.Models
 {
     public class LoginModel
     {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-
-        public DateTime Date { get; set; }
 
     }
 }
