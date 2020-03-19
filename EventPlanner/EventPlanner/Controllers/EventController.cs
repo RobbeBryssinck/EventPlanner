d46c0db86a8277a629688a810ae72092048c2012
Finn <finnlowlevel@gmail.com>
2020-03-19 14:19:27 +0100

    Final?

--- EventPlanner/EventPlanner/Controllers/EventController.cs
+++ EventPlanner/EventPlanner/Controllers/EventController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventPlanner.Data;
 using EventPlanner.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +10,35 @@ namespace EventPlanner.Controllers
 {
     public class EventController : Controller
     {
+
+        private EventPlannerContext db;
+
+        public EventController(EventPlannerContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult EventPage()
         {
             return View();
         }
 
-        public IActionResult CreateEvent()
+        public ActionResult CreateEvent()
         {
+            return View(new Event());
+        }
+        
+        [HttpPost]
+        public ActionResult CreateEvent(Event model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(model);
+                db.SaveChanges();
+            }
+
             return View();
         }
-
     }
 }
