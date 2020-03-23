1bac2b16a163ad865f275167b5e9929020a4f6be
Finn <finnlowlevel@gmail.com>
2020-03-23 10:56:46 +0100

    teem

--- EventPlanner/EventPlanner/Models/Event.cs
+++ EventPlanner/EventPlanner/Models/Event.cs
@@ -11,18 +11,26 @@ namespace EventPlanner.Models
         [Key]
         public int EventId { get; set; }
 
+        [Required]
         public string EventName { get; set; }
 
+        [Required]
         public DateTime Date { get; set; }
 
+        [Required]
         public int VisitorLimit { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string Location { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter a vaid e-mailadres")]
         public string Email { get; set; }
 
+        [Required]
         public string ImageSrc { get; set; }
     }
 }
