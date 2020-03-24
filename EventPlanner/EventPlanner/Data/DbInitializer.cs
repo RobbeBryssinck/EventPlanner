b9040094c52c3f20caa5c0fe845177242c98a0f8
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-03-24 15:57:59 +0100

    Added more data

--- EventPlanner/EventPlanner/Data/DbInitializer.cs
+++ EventPlanner/EventPlanner/Data/DbInitializer.cs
@@ -20,8 +20,34 @@ namespace EventPlanner.Data
 
             var events = new Event[]
             {
-                new Event{EventName="ASP.NET Core cursus",Date=DateTime.Parse("2020-03-16"),VisitorLimit=100,
-                    Description="Lorem ipsum",Location="Eindhoven",Email="test@test.com",ImageSrc="test.jpg"},
+                new Event{EventName="Bordspellenavond!",
+                    Date=DateTime.Parse("2020-04-03"),VisitorLimit=50,
+                    Description="Ja hoor!! We zetten onverminderd door met de mega-succesvolle bordspellenavond… " +
+                        "Neem je spellen mee, en je partner, vriend of zus. Eet je mee? Laat dit dan even weten aan onze ultieme spelleider Sven Klaassen Bos!",
+                    Location="Eindhoven",Email="test@test.com",ImageSrc="test1.jpg"},
+
+                new Event{EventName="Pizzasessie IT Rockstar Sven Haster | Four easy steps to migrate to JPMS",
+                    Date=DateTime.Parse("2020-04-15"),VisitorLimit=40,
+                    Description="Vanuit Chapter East introduceren wij IT Rockstar Sven Haster!! Op 15 April aanstaande zal hij zijn sessie “Four easy steps " +
+                        "to migrate to JPMS” geven in Jules Verne in Apeldoorn. Hier wil je bij zijn! Java 9 introduceerde het Java module system, ook wel project " +
+                        "Jigsaw genoemd. Voor veel ontwikkelaars is dit nog een abstract en ondoorzichtig geheel en dit is dan ook dé reden dat veel projecten nog op Java 8 zitten. " +
+                        "In deze presentatie laat IT Rockstar Sven vier simpele stappen zien om je project JPMS compliant te maken en naar Java 9+ te migreren.",
+                    Location="Apeldoorn",Email="test@test.com",ImageSrc="test2.jpg"},
+
+                new Event{EventName="IT Rockstar Christiaan Nieuwlaat | Modulaire Software Ontwikkeling – Chapter Lower South",
+                    Date=DateTime.Parse("2020-04-16"),VisitorLimit=50,
+                    Description="And he does it again!! Op 16 April zal IT Rockstar een sessie verzorgen over Modulaire Software Ontwikkeling. " +
+                        "Dit doet hij in het vertrouwde thuishonk van chapter Lower South; Doloris in Tilburg. Steeds meer systemen worden op een manier opgebouwd zodat deze " +
+                        "bestaan uit “herbruikbare” onderdelen. Dit gebeurt op macro niveau, het ombouwen van een monoliet naar microservices, maar ook op micro niveau, het " +
+                        "gebruik maken van modules als bibliotheken, bijvoorbeeld in de vorm van java 9+ modules. We gaan het hebben over deze verschillende niveaus, waarom we " +
+                        "gebruik willen maken van modulaire ontwikkeling en wat de voor- en nadelen hiervan zijn.",
+                    Location="Tilburg",Email="test@test.com",ImageSrc="test3.jpg"},
+
+                new Event{EventName="Pizzasessie IT Rockstar Bart Kardol | Digitalisering verandert de wereld, ook op zee",
+                    Date=DateTime.Parse("2020-04-22"),VisitorLimit=50,
+                    Description="n chapter Upper South hebben we IT Rockstar gepland staan op 22 April voor een supervette sessie: “Digitalisering verandert de wereld, ook op zee”. " +
+                    "Catchy titel, nietwaar? Inschrijven voor zijn sessie doe je hier! Het event vindt plaats op Team Rockstars IT HQ in ‘s-Hertogenbosch!",
+                    Location="Den Bosch",Email="test@test.com",ImageSrc="test4.jpg"},
             };
             foreach (Event e in events)
             {
