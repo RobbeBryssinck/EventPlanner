bc420ac40a4c3790b951cdb865c4698d01656ff4
TimonC <58507497+TimonC@users.noreply.github.com>
2020-03-26 11:01:01 +0100

    De eerste 3 naam voor homepagina

--- EventPlanner/EventPlanner/Controllers/HomeController.cs
+++ EventPlanner/EventPlanner/Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EventPlanner.Models;
+using EventPlanner.Data;
 
 namespace EventPlanner.Controllers
 {
     public class HomeController : Controller
     {
+        private EventPlannerContext db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EventPlannerContext db)
         {
+            this.db = db;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var emp_data = db.Events.OrderBy(e => e.Date).ToList();
+            return View(emp_data.GetRange(0, 3));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
