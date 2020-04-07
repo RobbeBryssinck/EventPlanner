dd491cd981dccd396a46b9584bb809b0a0a3c105
TimonC <58507497+TimonC@users.noreply.github.com>
2020-04-07 10:24:52 +0200

    Compatability toegevoegd voor de archive, educational en recreation

--- EventPlanner/EventPlanner/Controllers/EventController.cs
+++ EventPlanner/EventPlanner/Controllers/EventController.cs
@@ -147,20 +147,29 @@ namespace EventPlanner.Controllers
         public IActionResult Educational()
         {
             List<Event> events = db.Events.Where(s => s.EventType == "Educational" && s.Date > DateTime.Now).ToList();
-
+            if (events.Count == 0)
+            {
+                return RedirectToAction("EventNotFound");
+            }
             return View(events);
         }
 
-        public IActionResult Recreational()
+        public IActionResult Recreation()
         {
-            List<Event> events = db.Events.Where(s => s.EventType == "Recreational" && s.Date > DateTime.Now).ToList();
-
+            List<Event> events = db.Events.Where(s => s.EventType == "Recreation" && s.Date > DateTime.Now).ToList();
+            if (events.Count == 0)
+            {
+                return RedirectToAction("EventNotFound");
+            }
             return View(events);
         }
         public IActionResult EventArchive()
         {
             List<Event> events = db.Events.Where(s => s.Date < DateTime.Now).ToList();
-
+            if (events.Count == 0)
+            {
+                return RedirectToAction("EventNotFound");
+            }
             return View(events);
         }
 
