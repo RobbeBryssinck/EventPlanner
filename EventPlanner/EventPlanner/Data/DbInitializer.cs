2f53ab8a9dd7dac2fe06d7178e103212fe623734
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-04-16 13:27:26 +0200

    Fixed nested for loop

--- EventPlanner/EventPlanner/Data/DbInitializer.cs
+++ EventPlanner/EventPlanner/Data/DbInitializer.cs
@@ -132,8 +132,11 @@ namespace EventPlanner.Data
             foreach (Rating rating in ratings)
             {
                 context.Ratings.Add(rating);
-                var registrations = new Registration[]
-                {
+            }
+            context.SaveChanges();
+
+            var registrations = new Registration[]
+            {
                 new Registration{AccountId=1, EventId=2},
                 new Registration{AccountId=5, EventId=3},
                 new Registration{AccountId=2, EventId=1},
@@ -144,13 +147,12 @@ namespace EventPlanner.Data
                 new Registration{AccountId=5, EventId=1},
                 new Registration{AccountId=1, EventId=4},
                 new Registration{AccountId=4, EventId=2}
-                };
-                foreach (Registration registration in registrations)
-                {
-                    context.Registrations.Add(registration);
-                }
-                context.SaveChanges();
+            };
+            foreach (Registration registration in registrations)
+            {
+                context.Registrations.Add(registration);
             }
+            context.SaveChanges();
         }
     }
 }
