167a2cd76e3dc191f68f3bac07b64597a46c672d
Davidchang24 <57895305+Davidchang24@users.noreply.github.com>
2020-04-20 11:14:41 +0200

    Maps database input fix

--- EventPlanner/EventPlanner/Data/DbInitializer.cs
+++ EventPlanner/EventPlanner/Data/DbInitializer.cs
@@ -23,7 +23,7 @@ namespace EventPlanner.Data
                     Date=DateTime.Parse("2020-04-03"),VisitorLimit=50,
                     Description="Ja hoor!! We zetten onverminderd door met de mega-succesvolle bordspellenavond… " +
                         "Neem je spellen mee, en je partner, vriend of zus. Eet je mee? Laat dit dan even weten aan onze ultieme spelleider Sven Klaassen Bos!",
-                    Location="Eindhoven,Rachelsmolen1,5612MA",
+                    Location="Eindhoven,Rachelsmolen1",
                     Email="shout@teamrockstars.nl",ImageSrc="Bordspel.jpg", EventType = EventType.Recreation},
 
                 new Event{EventName="Pizzasessie IT Rockstar Sven Haster | Four easy steps to migrate to JPMS",
@@ -32,7 +32,7 @@ namespace EventPlanner.Data
                         "to migrate to JPMS” geven in Jules Verne in Apeldoorn. Hier wil je bij zijn! Java 9 introduceerde het Java module system, ook wel project " +
                         "Jigsaw genoemd. Voor veel ontwikkelaars is dit nog een abstract en ondoorzichtig geheel en dit is dan ook dé reden dat veel projecten nog op Java 8 zitten. " +
                         "In deze presentatie laat IT Rockstar Sven vier simpele stappen zien om je project JPMS compliant te maken en naar Java 9+ te migreren.",
-                    Location="Apeldoorn,Laanvandemensenrechten,7331VX,362",
+                    Location="Apeldoorn,Laanvandemensenrechten362",
                     Email="shout@teamrockstars.nl",ImageSrc="SvenHaster.jpg", EventType = EventType.Educational},
 
                 new Event{EventName="IT Rockstar Christiaan Nieuwlaat | Modulaire Software Ontwikkeling – Chapter Lower South",
@@ -42,14 +42,14 @@ namespace EventPlanner.Data
                         "bestaan uit “herbruikbare” onderdelen. Dit gebeurt op macro niveau, het ombouwen van een monoliet naar microservices, maar ook op micro niveau, het " +
                         "gebruik maken van modules als bibliotheken, bijvoorbeeld in de vorm van java 9+ modules. We gaan het hebben over deze verschillende niveaus, waarom we " +
                         "gebruik willen maken van modulaire ontwikkeling en wat de voor- en nadelen hiervan zijn.",
-                    Location="Tilburg,Spoorlaan,5038CB,26",
+                    Location="Tilburg,Spoorlaan26",
                     Email="shout@teamrockstars.nl",ImageSrc="ChristiaanNieuwlaat.jpg", EventType = EventType.Educational},
 
                 new Event{EventName="Pizzasessie IT Rockstar Bart Kardol | Digitalisering verandert de wereld, ook op zee",
                     Date=DateTime.Parse("2020-04-22"),VisitorLimit=50,
                     Description="n chapter Upper South hebben we IT Rockstar gepland staan op 22 April voor een supervette sessie: “Digitalisering verandert de wereld, ook op zee”. " +
                     "Catchy titel, nietwaar? Inschrijven voor zijn sessie doe je hier! Het event vindt plaats op Team Rockstars IT HQ in ‘s-Hertogenbosch!",
-                    Location="DenBosch,Lekkerbeetjesstraat,5511AL,5",
+                    Location="DenBosch,Lekkerbeetjesstraat5",
                     Email="shout@teamrockstars.nl",ImageSrc="BartKardol.jpg", EventType = EventType.Educational},
             };
             foreach (Event e in events)
