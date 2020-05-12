26da86c7b07d514b4d77c275377b78a7044b23e0
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-05-12 17:23:21 +0200

    Role creation

--- EventPlanner/EventPlanner/Controllers/AdminController.cs
+++ EventPlanner/EventPlanner/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using EventPlanner.Data;
 using EventPlanner.Models;
@@ -9,6 +10,7 @@ using EventPlanner.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 
@@ -18,13 +20,54 @@ namespace EventPlanner.Controllers
     public class AdminController : Controller
     {
         private EventPlannerContext db;
+        private readonly RoleManager<IdentityRole> roleManager;
         private IWebHostEnvironment _environment;
 
 
-        public AdminController(EventPlannerContext db, IWebHostEnvironment environment)
+        public AdminController(EventPlannerContext db, RoleManager<IdentityRole> roleManager, IWebHostEnvironment environment)
         {
             this.db = db;
+            this.roleManager = roleManager;
             this._environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
         }
 
         public IActionResult AdminAccountPage()
