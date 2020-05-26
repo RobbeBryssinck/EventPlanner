6216e1962c784e66479f3fc40bd7b3ce895b9cbc
dennycox <denny.cox@chello.nl>
2020-05-26 10:30:57 +0200

    Removed add account admin function, fixed wrong text and improved translation

--- EventPlanner/EventPlanner/ViewModels/AccountChangeViewModel.cs
+++ EventPlanner/EventPlanner/ViewModels/AccountChangeViewModel.cs
@@ -11,18 +11,22 @@ namespace EventPlanner.ViewModels
         public string id { get; set; }
 
         [Required]
+        [Display(Name = "Gebruikersnaam")]
         public string Username { get; set; }
 
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Huidig wachtwoord")]
         public string CurrentPassword { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Nieuw wachtwoord")]
         public string Password { get; set; }
     }
 }
