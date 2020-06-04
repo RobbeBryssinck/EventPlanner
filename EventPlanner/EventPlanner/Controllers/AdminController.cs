25d5c63bd3f37eca1f0ae6df51e769c46230c948
dennycox <denny.cox@chello.nl>
2020-06-04 13:45:02 +0200

    Improved role views and removed create role function

--- EventPlanner/EventPlanner/Controllers/AdminController.cs
+++ EventPlanner/EventPlanner/Controllers/AdminController.cs
@@ -32,38 +32,6 @@ namespace EventPlanner.Controllers
         }
 
         [HttpGet]
-        public IActionResult CreateRole()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                IdentityRole identityRole = new IdentityRole()
-                {
-                    Name = model.RoleName
-                };
-
-                IdentityResult result = await roleManager.CreateAsync(identityRole);
-
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-
-                foreach (IdentityError error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
         public IActionResult ListRoles()
         {
             var roles = roleManager.Roles;
