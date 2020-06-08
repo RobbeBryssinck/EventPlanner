6451a502df00fe2af3567bc6ea883fb7480a5237
Davidchang24 <57895305+Davidchang24@users.noreply.github.com>
2020-06-08 11:40:44 +0200

    Viewmodel mappen toegevoegd

---
+++ EventPlanner/EventPlanner/ViewModels/Admin/AdminAccountPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventPlanner.Models;
+
+namespace EventPlanner.ViewModels
+{
+    public class AdminAccountPageViewModel
+    {
+        public List<ApplicationUser> Users { get; set; }
+    }
+}
